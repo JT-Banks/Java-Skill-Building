d44a0b5e2905ac2fe88fe34852a37404fabcbfa4
JT <jbank54@lsu.edu>
2021-01-03 19:05:43 -0600

    Added comments to better explain coding problem and desired output.

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -3,12 +3,11 @@ import java.util.HashMap;
 public class CodingBat {
 
 	public static void main(String[] args) {
-			
+		
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		map.put("a", 10);
 		map.put("attack", 500);
 		map.put("defend", 0);
-		System.out.println(map.get("attack"));
-		
+		System.out.println(map.get("attack"));		
 	}
 }
--- src/Maps.java
+++ src/Maps.java
@@ -3,7 +3,7 @@ import java.util.Map;
 public class Maps {
 
 	public Map<String, String> mapBully(Map<String, String> map) {
-		
+	
 		return null;
 	}
 }
